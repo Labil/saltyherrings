2e4bfc782282c90bd77cd017e84aedc197060afc
Solveig Hansen <solveig.hansen@gmail.com>
2014-01-16 11:34:14 +0100

    Started adding Angular. Commiting to save before rewriting a lot in main.js

--- main.js
+++ main.js
@@ -11,6 +11,19 @@
 //		- Add side panel to site for info display and name choice & search
 //		- Make sprite sheet in stead of separate images.
 
+var app = angular.module('app', []);
+
+app.controller('AppCtrl', function(){
+	this.defaultMsg = "Use the search bar to lookup a salty herring by name, or click on the ones swimming around in the tank!";
+	//The info message changes, default set to defaultMsg:
+	this.info = this.defaultMsg;
+	this.searchDefault = "Search for herring";
+});
+
+function PanelCtrl($scope, SearchBox){
+	$scope.searchBox = SearchBox;
+}
+
 var setupRenderContext = function(){
 	var chopOff = 50;
 	canvas = document.createElement("canvas");
@@ -37,20 +50,14 @@ var setupEventListeners = function(){
 				&& e.clientY > hY 
 				&& e.clientY < hY + hH){
 
-				if(selectedHerring){
-					selectedHerring.deselect();
-				}
-				selectedHerring = fishbowl.herrings[i];
-				selectedHerring.select();
+				selectHerring(fishbowl.herrings[i]);
 				hit = true;
 				break;
 			}
 		}
 		if(hit == false){
-			if(selectedHerring){
-				selectedHerring.deselect();
-				selectedHerring = null;
-			}
+			deselectHerring();
+
 			if(e.clientX < canvasZeroPos + canvas.width 
 				&& e.clientX > canvasZeroPos 
 				&& e.clientY < canvas.height-marginY 
@@ -61,24 +68,39 @@ var setupEventListeners = function(){
 	}, false);
 };
 
+var selectHerring = function(herring){
+	deselectHerring();
+	selectedHerring = herring;
+	selectedHerring.select();
+	//app.info = "Hi my property is: " + selectedHerring.property;
+};
+
+var deselectHerring = function(){
+	if(selectedHerring){
+		selectedHerring.deselect();
+		selectedHerring = null;
+	}
+};
+
 var setupSidePanel = function(){
-	panel = document.createElement("div")
+	//panel = document.createElement("div")
 	//Attributes for panel set in "saltystyles.css"
-	panel.setAttribute("id", "panel");
-	document.body.appendChild(panel);
+	//panel.setAttribute("id", "panel");
+	//document.body.appendChild(panel);
 	//Init some variables
-	panelW = getElementAttr(panel, "width");
+	panelW = getElementAttr("panel", "width");
 	canvasZeroPos = panelW;
 };
 
-//elem is the DOM element, attr is a string attribute
+//elem is string of id, attr is a string attribute
 var getElementAttr = function(elem, attr){
-	var style = window.getComputedStyle(elem);
+	var elm = document.getElementById(elem);
+	var style = window.getComputedStyle(elm);
 	var value = style.getPropertyValue(attr);
 	return parseInt(value);
 };
 
-var fishbowl = new Fishbowl();
+var fishbowl;
 var selectedHerring = null;
 var canvas, context, panel;
 //window.innerWidth/Height is the most agreed upon (by the different browsers) 
@@ -88,7 +110,13 @@ var pageH = window.innerHeight;
 var panelW;
 var canvasZeroPos;
 
-setupSidePanel();
-setupRenderContext();
-fishbowl.addRenderContext(canvas.width, canvas.height, context);
-setupEventListeners();
+$(function() {
+	fishbowl = new Fishbowl();
+
+	setupSidePanel();
+	setupRenderContext();
+	fishbowl.addRenderContext(canvas.width, canvas.height, context);
+	setupEventListeners();
+});
+
+
